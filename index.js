04e396dcfc7702697168a4c69d2174437e1cf51a
Denise Justice <denise@devpipeline.com>
2023-03-29 12:09:33 -0600

    Capstone submission Unfinished

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ async function grabStudents() {
   }
 }
 
-//   customerData.forEach (x =>  {
-//     console.log()
-
-// fetch("MOCK_DATA.json")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
 function add() {
   const plusSymbol = document.getElementById("plusSymbol");
   const minus = parseInt(plusSymbol.value) + 1;
   plusSymbol.value = minusSymbol;
+
+  weightVal(plusSymbol);
 }
 
 function subtract() {
@@ -27,24 +22,6 @@ function subtract() {
   const minus = parseInt(plusSymbol.value) - 1;
   plus.value = minusSymbol;
 }
-
-// let plusSymbol = document.querySelector("#plusSymbol");
-// plusSymbol.addEventListener("click", function () {
-//   let output = document.querySelector("output");
-//   let result = parseInt(output.innerText) + 1;
-//   // had to add in word number to not make it a string because everytime I hit the + button it would add a number 1.
-//   output.innerText = result;
-// });
-
-/* 
-<div class="user-wrapper"> [x]
-              <h3>Denise J. : <span>1</span></h3> [x]
-              <div class="btn-container">
-                <button>+</button>
-                <button>-</button>
-              </div>
-            </div>
-*/
 
 const mockData = {
   message: "Logged In",
@@ -82,7 +59,14 @@ const mockData = {
       cohort: 4,
       __v: 0,
     },
-
+    {
+      _id: "6418c311e0d2fe0f4f6dd5e9",
+      first_name: "Denise",
+      last_name: "Justice",
+      role: "standard",
+      cohort: 4,
+      __v: 0,
+    },
     {
       _id: "6418c3a8e0d2fe0f4f6dd618",
       first_name: "Karely",
@@ -142,20 +126,39 @@ const mockData = {
   ],
 };
 
+// let animateMe = document.getElementById("animateMe");
+
+// function animateChars(ehchOne) {
+//   let echOnelements = element.querySelector("h1");
+
+// }
+// animateChars(animateMe);
+
 function firstAndLast(oMockData) {
   return `${
     oMockData.first_name[0].toUpperCase() + oMockData.first_name.slice(1)
   } ${oMockData.last_name.slice(0, 1).toUpperCase()} : `;
 }
 
+const studentArray = [];
 mockData.users.forEach((user) => {
   createUsers(user);
 });
 
-function weightVal(weightName) {
+function weightVal(animateName) {
   document.getElementById("main-container").innerHTML = val * val;
 }
-weightVal(plusSymbol);
+
+function onClick(somethingGoesHere) {
+  let addBtn, subBtn, result;
+  addBtn = document.getElementById("addButton").value = "+";
+  subBtn = document.getElementById("subButton").value = "-";
+  document.getElementById("onClick");
+  generatorButton.addEventListener("click", onClick);
+
+  const generatorButton = document.getElementById("generatorButton");
+  generatorButton.addEventListener("click", onClick);
+}
 
 function createUsers(user) {
   const usersContainer = document.createElement("div");
@@ -164,9 +167,12 @@ function createUsers(user) {
   const subBtn = document.createElement("button");
   const nameHeader = document.createElement("h3");
   const weightSpan = document.createElement("span");
+
   const nameNode = document.createTextNode(firstAndLast(user));
 
   const spanNumber = document.createTextNode("1");
+
+  console.log(nameHeader, usersContainer);
 
   // User Data
   usersContainer.classList.add("user-wrapper");
@@ -187,3 +193,35 @@ function createUsers(user) {
 
   usersEl.appendChild(usersContainer);
 }
+
+function shuffleName() {
+  const theIndex = math.floor(math.random() * mockData.users.length);
+  const indexStudent = mockData.users[indexStudent];
+  usersContainer.textContent = firstAndLast(indexStudent);
+}
+
+function shuffleTime() {
+  shuffleSet = shuffleTime(() => {
+    shuffleName();
+  }, 5000);
+  return new promise((res, reje) => {
+    setTimeout(() => {
+      clearSettings(shuffleSet);
+    });
+  });
+}
+
+// let plusSymbol = document.querySelector("#plusSymbol");
+// plusSymbol.addEventListener("click", function () {
+//   let output = document.querySelector("output");
+//   let result = parseInt(output.innerText) + 1;
+
+//   output.innerText = result;
+// });
+
+//   customerData.forEach (x =>  {
+//     console.log()
+
+// fetch("MOCK_DATA.json")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
